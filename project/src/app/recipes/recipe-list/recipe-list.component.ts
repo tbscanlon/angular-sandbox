4cf9078bc5345163985270303776e4282ecdec47
tbscanlon <thomas.scanlon@outlook.com>
2017-10-20 17:59:50 +0100

    Create project skeleton

---
+++ project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test Recipe',
+      'Just another test recipe',
+      'http://ghk.h-cdn.co/assets/16/38/980x490/landscape-1474822198-how-to-make-pancakes.jpg'),
+      new Recipe(
+        'Normal Pancakes',
+        'Boring normal pancakes',
+        'http://ghk.h-cdn.co/assets/16/38/980x490/landscape-1474822198-how-to-make-pancakes.jpg'),
+      new Recipe(
+        'Cool Pancakes',
+        'Cool unique pancakes',
+        'http://ghk.h-cdn.co/assets/16/38/980x490/landscape-1474822198-how-to-make-pancakes.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
